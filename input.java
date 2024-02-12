12910eb2527a23a4d3ef21815ffcc79cbe2b4f41
manasvi1512 <manasvij424@gmail.com>
2024-02-12 18:18:37 +0530

    input ways

--- input.java
+++ input.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
 public class input {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Id");
+        int id = scanner.nextInt();
+        scanner.nextLine(); 
+        String name = scanner.nextLine();
     }
 }
