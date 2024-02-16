f220f16e1af46a042483c6c29036b19ec1e77bf5
manasvi1512 <manasvij424@gmail.com>
2024-02-16 11:51:51 +0530

    update files

---
+++ Fliptheposition.java
@@ -0,0 +1,16 @@
+public class Fliptheposition {
+    public static void main(String[] args) {
+        //flip the position with digit
+        int n = 32145;
+        int a=n;
+        int pos= 0;
+        double sum=0;
+        while(n>0) {
+            int d=n%10;
+            pos++;
+            sum= sum+ pos*(Math.pow(10,d-1));
+            n=n/10;
+        }
+        System.out.println("Flipping the position of "+a+ " gives " +(int)sum);
+    }
+}
